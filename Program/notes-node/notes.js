3593be45d9e0b91afb57586eb3c75e576a9f0ea2
Devidas Dhene <devidasd@PALASH.COM>
2018-05-17 18:47:46 +0530

    d

--- Program/notes-node/notes.js
+++ Program/notes-node/notes.js
@@ -1,7 +1,27 @@
 console.log("Notes Js Starting")
 //module.exports.age=24;
+const fs=require('fs');
 var addNote=(title,body)=>{
-  console.log("Adding ",title,body);
+ var notes=[];
+ var note={
+ 	title,body
+ };
+ try{
+ var noteString=	fs.readFileSync('note-data.json');
+  var notes=JSON.parse(noteString);
+ }
+
+ // var duplicateArray=notes.filter((note)=>{
+ // 	return note.title===title;
+ // });
+ var duplicateArray=notes.filter((note)=>note.title===title);
+
+ if(duplicateArray.length===0){
+ 	notes.push(note);
+ 	fs.writeFileSync('note-data.json',notes);
+ }
+
+
  
 };
 var getAll=()=>{
