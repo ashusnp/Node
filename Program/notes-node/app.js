9196827a8d96c93e890f55eaa7ad3d52b7e6b239
Ashish <ashusnp@gmail.com>
2018-05-17 13:48:00 +0530

    11

--- Program/notes-node/app.js
+++ Program/notes-node/app.js
@@ -1,11 +1,17 @@
 console.log("Start");
 const fs=require('fs');
 const os=require('os');
-const notes=require('./notes.js')
-var res=notes.addNote();
-console.log(res);
+const _=require('lodash');
+const notes=require('./notes.js');
+//var res=notes.addNote();
+//console.log(os.userInfo());
 
-console.log('Result:', notes.add(9,-2));
+//console.log('Result:', notes.add(9,-2));
+
+console.log("Lodash ",_.isString(true));
+console.log("Lodash ",_.isString("Ashsh"));
+var filterArray=_.uniq(["Ashish",1,"Ashish",1,2,3,4]);
+console.log(filterArray);
 
 
 // var user=os.userInfo();
