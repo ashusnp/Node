a01aae5a0fe399c8ad8ae901d4b8e1340aafcac0
Ashish <ashusnp@gmail.com>
2018-05-23 23:21:29 +0530

    30

--- Program/weather-app/app.js
+++ Program/weather-app/app.js
@@ -4,5 +4,8 @@ request({
   url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC2Wf8xPQw_16jiKIYS8yYo2PEP6J3FgqE&address=%20443302%20lonar',
   json:true
 },(error,response,body)=>{
-  console.log(body);
+  //console.log(JSON.stringify(body,undefined,2));
+  console.log(`Address :${body.results[0].formatted_address}`);
+  console.log(`lat :${body.results[0].geometry.location.lat}`);
+    console.log(`lng :${body.results[0].geometry.location.lng}`);
 });
