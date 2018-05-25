cecb28914eb2848b1533f0b117c66861e86bea59
ashish sanap <ashishs@PALASH.COM>
2018-05-25 18:29:33 +0530

    dasa

--- Program/weather-app/playground/promise.js
+++ Program/weather-app/playground/promise.js
@@ -1,3 +1,23 @@
+var asyncAdd=(a,b)=>{
+  return new Promise((resolve,reject)=>{
+    if(typeof a==='number' && typeof b==='number'){
+      resolve(a+b);
+    }
+    else{
+      reject('Arguments must be a number');
+    }
+  });
+};
+
+asyncAdd(3,5).then((res)=>{
+  console.log('Result ',res)
+  return asyncAdd(res,12);
+}).then((res1)=>{
+  console.log('Result 1 ',res1);
+}).catch((errorMessage1)=>{
+console.log(errorMessage1);
+});
+
 somePromise=new Promise((resolve,reject)=>{
   setTimeout(()=>{
 //    resolve('Hey I Am Promise');
