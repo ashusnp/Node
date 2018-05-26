1cf3333bd4f1eb642fdcb25bea78c665eeb00fb1
Ashish <ashusnp@gmail.com>
2018-05-26 19:53:12 +0530

    43 HBS module to render views

--- Program/node-web-server/server.js
+++ Program/node-web-server/server.js
@@ -1,23 +1,33 @@
 const express=require('express');
+const hbs=require('hbs');
 
 
 var app=express();
-
+app.set('view engine','hbs');
 //console.log(__dirname);
 app.use(express.static(__dirname+'/public'));
 app.get('/',(request,response)=>{
   //response.send("Hello world");
-  response.send({
-    name:'Ashish',
-    about:{
-      age:24,
-      job:'full stack developer'
-    }
-  });
+  // response.send({
+  //   name:'Ashish',
+  //   about:{
+  //     age:24,
+  //     job:'full stack developer'
+  //   }
+  // });
+
+  response.render('home.hbs',{
+    pageTitle:"Home",
+    currentYear:new Date().getFullYear()
+  })
 });
 
 app.get('/about',(request,response)=>{
-  response.send('<h1>About Page</h1>')
+//  response.send('<h1>About Page</h1>')
+response.render('about.hbs',{
+  pageTitle:'About',
+  currentYear:new Date().getFullYear()
+});
 });
 
 app.get('/bad',(request,response)=>{
