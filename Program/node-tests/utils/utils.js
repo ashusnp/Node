0c2d33650708d44342b0d0f50a4c24fb8d332398
Ashish <ashusnp@gmail.com>
2018-05-31 23:37:18 +0530

    53 expect assert start

--- Program/node-tests/utils/utils.js
+++ Program/node-tests/utils/utils.js
@@ -1,4 +1,12 @@
 module.exports.add=(a,b)=>{
   return a+b;
 }
-module.exports.square=(x)=>x*x 
+module.exports.square=(x)=>x*x
+module.exports.setUser=(user,fullName)=>{
+  var arr=fullName.split(" ");
+  //console.log(arr);
+  user={};
+  user.firstName=arr[0];
+  user.lastName=arr[1];
+  return user;
+}
--- Program/node-tests/utils/utils.test.js
+++ Program/node-tests/utils/utils.test.js
@@ -1,15 +1,32 @@
 const utils=require('./utils');
+const expect=require('expect');
 
 it('should add two number',()=>{
   var res=utils.add(10,10);
-  if(res!==20){
-    throw new Error(`Result should be 20 but ${res} got`);
-  }
+  expect(res).toBe(20).toBeA('number');//https://github.com/mjackson/expect
+  // if(res!==20){
+  //   throw new Error(`Result should be 20 but ${res} got`);
+  // }
 });
 
 it('should square a number',()=>{
   var res=utils.square(2);
-  if(res!==4){
-      throw new Error(`Result should be 4 but ${res} got`);
-  }
+  expect(res).toBe(4).toBeA('number');
+  // if(res!==4){
+  //     throw new Error(`Result should be 4 but ${res} got`);
+  // }
+});
+it('should expect some values',()=>{
+//expect(112).toNotBe(12);//check for not equal values
+//expect({name:'Ashish'}).toEqual({name:'Ashish'});//check for object to be equal
+//expect([1,2,3]).toInclude(1);//check if array contains element;
+//expect([1,2,3]).toExclude(1);//check if array not contains element
+expect({name:'Ashish',age:24}).toInclude({age:24});//check if object match the value or not
+});
+it('check user fullname',()=>{
+  var user=utils.setUser(user,"Ashish Sanap");
+  //console.log(user);
+expect(user).toEqual(
+  {firstName:'Ashish',
+  lastName:'Sanap'});
 });
